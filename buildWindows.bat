889cd686f8585dd4d15337a811ec2377fe9d5cb0
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-09 17:59:29 +0200

    Test env output

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,5 +1,7 @@
 @echo on
 echo Starting build
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+echo Set
+Set
 C:\Strawberry\perl\bin\perl.exe Configure VC-WIN64A
 nmake
