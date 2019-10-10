68594b4a8b390325a8bf8f2720287f38c9eaf401
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-10 17:18:34 +0200

    Install to C:\Users\travis\build\Qt

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,9 +1,8 @@
 @echo on
 echo Starting build
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-.\configure.bat -release -opensource -confirm-license -prefix "C:\projects\Qt" -opengl dynamic -platform win32-msvc2015 -openssl "-IC:\projects\openSSL\include" "-LC:\projects\openSSL" -nomake examples -nomake tests -no-direct2d
+.\configure.bat -release -opensource -confirm-license -prefix "C:\Users\travis\build\Qt" -opengl dynamic -platform win32-msvc2015 -openssl "-IC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL\include" "-LC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL" -nomake examples -nomake tests -no-direct2d
 where nmake
 where jom
 jom
-# Install Qt to "C:\projects\Qt"
 jom install
