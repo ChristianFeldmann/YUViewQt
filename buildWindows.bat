40ba3e6df489d110cbe7eac52e977a299fa6c8a7
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-11 12:35:30 +0200

    Use call for bat file

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,7 +1,7 @@
 @echo on
 echo Starting build
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-.\configure.bat -release -opensource -confirm-license -prefix "C:\Users\travis\build\Qt" -opengl dynamic -platform win32-msvc2015 -openssl "-IC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL\include" "-LC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL" -nomake examples -nomake tests -no-direct2d
+call .\configure.bat -release -opensource -confirm-license -prefix "C:\Users\travis\build\Qt" -opengl dynamic -platform win32-msvc2015 -openssl "-IC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL\include" "-LC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL" -nomake examples -nomake tests -no-direct2d
 echo Where
 where jom
 echo Call jom
