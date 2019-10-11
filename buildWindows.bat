4ef5d92e9e2dddc338a7d805af9f2dc2ee54ba74
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-11 09:30:13 +0200

    Weired error in windows: Try again

--- buildWindows.bat
+++ buildWindows.bat
@@ -2,7 +2,5 @@
 echo Starting build
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 .\configure.bat -release -opensource -confirm-license -prefix "C:\Users\travis\build\Qt" -opengl dynamic -platform win32-msvc2015 -openssl "-IC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL\include" "-LC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL" -nomake examples -nomake tests -no-direct2d
-where nmake
-where jom
 jom
 jom install
