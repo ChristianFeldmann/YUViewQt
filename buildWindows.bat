d568b26f4688daafc845b2178386d3bf6e0ad1b3
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-11 18:41:22 +0200

    Copy win_flex and win_bison to flex and bison

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,4 +1,7 @@
 @echo on
+echo Setup Winflexbison
+copy C:\ProgramData\chocolatey\bin\win_flex.exe C:\ProgramData\chocolatey\bin\flex.exe
+copy C:\ProgramData\chocolatey\bin\win_bison.exe C:\ProgramData\chocolatey\bin\bison.exe
 echo Starting build
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 call .\configure.bat -release -opensource -confirm-license -prefix "C:\Users\travis\build\Qt" -opengl dynamic -platform win32-msvc2015 -openssl "-IC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL\include" "-LC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL" -nomake examples -nomake tests -no-direct2d
