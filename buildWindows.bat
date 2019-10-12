e76fdb1aeca7219cd7a5d8dfe3b9069b2ea852d7
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-12 17:35:21 +0200

    Where is perl? Which version?

--- buildWindows.bat
+++ buildWindows.bat
@@ -4,6 +4,9 @@ copy C:\ProgramData\chocolatey\bin\win_flex.exe C:\ProgramData\chocolatey\bin\fl
 copy C:\ProgramData\chocolatey\bin\win_bison.exe C:\ProgramData\chocolatey\bin\bison.exe
 echo Delete wrong perl version
 del C:\program files\Git\usr\bin\perl.exe
+echo Where perl
+where perl
+perl --version
 echo Starting build
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 call .\configure.bat -release -opensource -confirm-license -prefix "C:\Users\travis\build\Qt" -opengl dynamic -platform win32-msvc2015 -openssl "-IC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL\include" "-LC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL" -nomake examples -nomake tests -no-direct2d
