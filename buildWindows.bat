505c1f1fd2fe8480d0b5f06fd7f9fa23a31ce08e
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-13 16:57:06 +0200

    This works. Re-enable osx. Cleanups.

--- buildWindows.bat
+++ buildWindows.bat
@@ -2,17 +2,10 @@
 echo Setup Winflexbison
 copy C:\ProgramData\chocolatey\bin\win_flex.exe C:\ProgramData\chocolatey\bin\flex.exe
 copy C:\ProgramData\chocolatey\bin\win_bison.exe C:\ProgramData\chocolatey\bin\bison.exe
-echo Set
-Set
-echo Where perl
-where perl
-perl --version
-echo Starting build
+echo Configuring Qt...
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 call .\configure.bat -release -opensource -confirm-license -prefix "C:\Users\travis\build\Qt" -opengl dynamic -platform win32-msvc2015 -openssl "-IC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL\include" "-LC:\Users\travis\build\ChristianFeldmann\YUViewQt\openSSL" -nomake examples -nomake tests -no-direct2d
-echo Where
-where jom
-echo Call jom
+echo Start Qt Build using jom...
 jom
-echo Call jom install
+echo Install Qt into C:\Users\travis\build\Qt
 jom install
