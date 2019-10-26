1b7744ac3c7a40202903e3fa1d75d0839fbce96f
Christian Feldmann <christian.feldmann@bitmovin.com>
2019-10-26 16:03:27 +0200

    Cleanup + goto right directory

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,7 +1,4 @@
 @echo on
-echo Setup Winflexbison
-copy C:\ProgramData\chocolatey\bin\win_flex.exe C:\ProgramData\chocolatey\bin\flex.exe
-copy C:\ProgramData\chocolatey\bin\win_bison.exe C:\ProgramData\chocolatey\bin\bison.exe
 echo Qmake Tools
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 C:\Users\travis\build\Qt\bin\qmake.exe
